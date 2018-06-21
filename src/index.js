42ee2e7426e0e182dbecf91adda622ab531d9f8e
lwesy <lw@esyoil.com>
2018-06-21 11:21:14 +0200

    Add typographyjs to project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Typography from "typography";
 
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+
+const typography = new Typography({
+  baseFontSize: "18px"
+});
+
+typography.injectStyles();
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
