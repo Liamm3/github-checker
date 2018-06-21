1f76166a82ec7480fb55f11ad9583b8e2efc9cc7
lwesy <lw@esyoil.com>
2018-06-21 17:10:26 +0200

    Add a basic layout

--- src/container/Profile.js
+++ src/container/Profile.js
@@ -1,3 +1,28 @@
 import React from "react";
+import styled from "styled-components";
 
-export default () => <h1>Profile</h1>;
+import Heading from "../components/Heading";
+
+const Grid = styled.div`
+  display: grid;
+  height: 100%;
+  grid-template-columns: 1fr 3fr;
+  grid-template-areas: "about repos";
+`;
+
+const About = styled.div`
+  grid-area: "about";
+`;
+
+const Repos = styled.div`
+  grid-area: "about";
+`;
+
+export default ({ match }) => (
+  <Grid>
+    <About>
+      <Heading>{match.params.name}</Heading>
+    </About>
+    <Repos>Repos</Repos>
+  </Grid>
+);
